904a36d99dc14eab0835fa1da9814c53574bb490
bigboy-baby <63435309+bigboy-baby@users.noreply.github.com>
2022-08-17 19:08:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import httpx, random, string
+import aiohttp
+from aiohttp.client import ClientSession
+import asyncio
+
+count = 0
+
+async def addFriend(token, username, iden, am):
+  global count
+  r = httpx.post("https://nertivia.net/api/user/relationship", json={"username":username,"tag":iden}, headers={"authorization": token})
+  res = r.json()
+  count+=1
+  if res["status"] == True:
+    print(f"Sent Friend Request [{count}/{am}]")
+  else:
+    print(f"Failed to Send Friend Request [{count}/{am}]")
+
+async def loadtokens(tag):
+  split = tag.split(":")
+  username = split[0]
+  iden = split[1]
+
+  token_file = open('tokens.txt', 'r')
+  tokens = token_file.read()
+  tokenlist = tokens.split("\n")
+  am = len(tokenlist)
+  print(f"{am} Accounts Loaded")
+
+  for token in tokenlist:
+    await addFriend(token, username, iden, am)
+
+
+tag = input("User's Tag: ")
+asyncio.run(loadtokens(tag))
